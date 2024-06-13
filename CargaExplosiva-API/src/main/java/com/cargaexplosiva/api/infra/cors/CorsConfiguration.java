331cfe25f1b81786c25457e8393cdddb1baf8441
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-06-13 00:40:11 -0300

    Update CorsConfiguration.java

--- CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/infra/cors/CorsConfiguration.java
+++ CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/infra/cors/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        //.allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
                 // Enable CORS for the whole application.
