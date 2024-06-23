93b0b3c1fbee5e0257bd4b957bc97c822f14385e
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-06-23 19:38:02 -0300

    Update ControlePresencaController.java

--- CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/controller/ControlePresencaController.java
+++ CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/controller/ControlePresencaController.java
@@ -1,9 +1,16 @@
 package com.cargaexplosiva.api.controller;
 
+import com.cargaexplosiva.api.service.ControlePresencaService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("controlepresenca")
+@RequestMapping("bater-ponto")
 public class ControlePresencaController {
+
+    private final ControlePresencaService controlePresencaService;
+
+    public ControlePresencaController(ControlePresencaService controlePresencaService) {
+        this.controlePresencaService = controlePresencaService;
+    }
 }
