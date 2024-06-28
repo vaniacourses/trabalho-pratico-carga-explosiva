ead5b0c71bc4a22edd7abd853e8d2b9d8776d0ed
Caio-gc <88519569+Caio-gc@users.noreply.github.com>
2024-06-28 12:38:47 -0300

    Crud Gerente

--- CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/controller/GerenteController.java
+++ CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/controller/GerenteController.java
@@ -1,17 +1,13 @@
 package com.cargaexplosiva.api.controller;
 
-import com.cargaexplosiva.api.model.*;
-import com.cargaexplosiva.api.service.AtribuicaoVeiculoMotoristaService;
+import com.cargaexplosiva.api.dto.requestRegisterGerenteDTO;
+import com.cargaexplosiva.api.dto.requestUpdateGerenteDTO;
 import com.cargaexplosiva.api.service.GerenteService;
-import com.cargaexplosiva.api.service.MotoristaService;
-import com.cargaexplosiva.api.service.VeiculoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,4 +20,36 @@ public class GerenteController{
         this.gerenteService = gerenteService;
     }
 
+
+    @PostMapping
+    public ResponseEntity<Object> saveGerente(@RequestBody @Valid requestRegisterGerenteDTO gerenteDTO) {
+        try {
+            gerenteService.saveGerenteFrota(gerenteDTO); // Ou saveGerenteMecanico(gerenteDTO) conforme necessário
+            return ResponseEntity.status(HttpStatus.CREATED).body("Gerente criado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar gerente.");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getGerente(@PathVariable(value = "id") UUID id) {
+        return gerenteService.getById(id);
+    }
+
+
+    @GetMapping("/email")
+    public ResponseEntity<Object> getGerenteByEmail(@RequestParam String email) {
+        return gerenteService.getByEmail(email);
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateGerente(@RequestBody @Valid requestUpdateGerenteDTO gerenteDTO) {
+        return gerenteService.update(gerenteDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteGerente(@PathVariable(value = "id") UUID id) {
+        gerenteService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
