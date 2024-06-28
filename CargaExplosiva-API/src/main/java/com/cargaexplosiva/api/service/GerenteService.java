7ddcfb3e19a086cbfadc55f78cf27e56372aa42e
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-06-28 18:10:38 -0300

    Revert "Crud Gerente"
    This reverts commit ead5b0c71bc4a22edd7abd853e8d2b9d8776d0ed.

--- CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/service/GerenteService.java
+++ CargaExplosiva-API/src/main/java/com/cargaexplosiva/api/service/GerenteService.java
@@ -1,19 +1,13 @@
 package com.cargaexplosiva.api.service;
 
 import com.cargaexplosiva.api.dto.requestRegisterGerenteDTO;
-import com.cargaexplosiva.api.dto.requestUpdateGerenteDTO;
 import com.cargaexplosiva.api.model.FuncionarioRole;
 import com.cargaexplosiva.api.model.Gerente;
 import com.cargaexplosiva.api.repository.GerenteRepository;
 import org.springframework.beans.BeanUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @Transactional
@@ -39,39 +33,5 @@ public class GerenteService{
         gerente.setPassword(new BCryptPasswordEncoder().encode(gerentDTO.password()));
         gerente.setRole(FuncionarioRole.GERENTE_MECANICO);
         gerenteRepository.save(gerente);
-    }
-
-
-    public ResponseEntity<Object> getById(UUID id) {
-        Optional<Gerente> gerente = gerenteRepository.findById(id);
-        return gerente.<ResponseEntity<Object>>
-                        map(value -> ResponseEntity.status(HttpStatus.OK).body(value))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Gerente não encontrado."));
-    }
-
-    public ResponseEntity<Object> getByEmail(String email) {
-        Optional<Gerente> gerente = gerenteRepository.findByEmail(email);
-        return gerente.<ResponseEntity<Object>>
-                        map(value -> ResponseEntity.status(HttpStatus.OK).body(value))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Gerente não encontrado."));
-    }
-
-    public ResponseEntity<Object> update(requestUpdateGerenteDTO gerenteDTO) {
-        Optional<Gerente> gerenteOptional = gerenteRepository.findById(gerenteDTO.id_funcionario());
-        if (gerenteOptional.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Gerente não encontrado.");
-        }
-        var gerente = gerenteOptional.get();
-        BeanUtils.copyProperties(gerenteDTO, gerente, "id", "password", "role", "pedidos");
-        gerenteRepository.save(gerente);
-        return ResponseEntity.status(HttpStatus.OK).body(gerente);
-    }
-
-    public ResponseEntity<Object> deleteById(UUID id) {
-        if (!gerenteRepository.existsById(id)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Gerente não encontrado.");
-        }
-        gerenteRepository.deleteById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
