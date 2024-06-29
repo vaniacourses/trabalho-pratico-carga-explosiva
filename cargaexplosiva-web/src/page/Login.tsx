41d85308474662963324f52a9d2b84c4ce16b1ba
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2024-06-29 12:42:39 -0300

    acertando a página

--- cargaexplosiva-web/src/page/Login.tsx
+++ cargaexplosiva-web/src/page/Login.tsx
@@ -23,41 +23,43 @@ function Login() {
 
     return (
         <main>
-            <div className={'container text-center'}>
-                <form onSubmit={handleSubmit(login)}
-                      className={"text-center justify-content-center"}>
-                    <div className="form-floating mb-3">
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="emailInput"
-                            placeholder=""
-                            {...register("email")}
-                        />
-                        <label htmlFor="emailInput">
-                            Email</label>
-                        {errors.email &&
-                            <p className="text-error p-0 m-0">{errors.email.message}</p>}
-                    </div>
-                    <div className="form-floating">
-                        <input type="password"
-                               className="form-control"
-                               id="passwordInput"
-                               placeholder=""
-                               {...register("password")}
-                        />
-                        <label htmlFor="passwordInput">
-                            Senha</label>
-                        {errors.password &&
-                            <p className="text-error p-0 m-0">{errors.password.message}</p>}
-                    </div>
-                    <div className={"d-flex justify-content-end pt-3"}>
-                        <button type="submit"
-                                className="btn btn-outline-info">
-                            Entrar
-                        </button>
-                    </div>
-                </form>
+            <div className="page-login">
+                <div className='container-fluid text-center'>
+                    <form onSubmit={handleSubmit(login)}
+                          className={"text-center justify-content-center"}>
+                        <div className="form-floating mb-3">
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="emailInput"
+                                placeholder=""
+                                {...register("email")}
+                            />
+                            <label htmlFor="emailInput">
+                                Email</label>
+                            {errors.email &&
+                                <p className="text-error p-0 m-0">{errors.email.message}</p>}
+                        </div>
+                        <div className="form-floating">
+                            <input type="password"
+                                   className="form-control"
+                                   id="passwordInput"
+                                   placeholder=""
+                                   {...register("password")}
+                            />
+                            <label htmlFor="passwordInput">
+                                Senha</label>
+                            {errors.password &&
+                                <p className="text-error p-0 m-0">{errors.password.message}</p>}
+                        </div>
+                        <div className={"d-flex justify-content-end pt-3"}>
+                            <button type="submit"
+                                    className="btn btn-outline-info">
+                                Entrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </main>
     );
